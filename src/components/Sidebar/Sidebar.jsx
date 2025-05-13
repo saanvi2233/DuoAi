b995870145efc1f35387efc8ba3a077bc2099a1a
saanvi2233 <140193097+saanvi2233@users.noreply.github.com>
2025-05-13 12:39:03 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import { assets } from '../../assets/assets';
-import { Context } from '../../context/context';
+import { Context } from '../../context/Context';
 
 
 
